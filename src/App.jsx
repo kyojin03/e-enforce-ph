7b81da4e528a2ea6a5528371db25d6e31eb7b6b3
kyojin03 <pilbrnrdno@gmail.com>
2026-03-30 11:28:45 +0800

    updated about section

--- src/App.jsx
+++ src/App.jsx
@@ -253,7 +253,7 @@ export default function App() {
               style={{ width: "52px", height: "52px", borderRadius: "50%", objectFit: "cover" }}
             />
             <div>
-              <h1 style={{ margin: 0, color: "#15803d" }}>e-Enforce PH</h1>
+              <h1 style={{ margin: 0, color: "#15803d" }}>Piolo pogi</h1>
               <p style={{ margin: 0, fontSize: "13px", color: "#64748b" }}></p>
             </div>
           </div>
@@ -673,7 +673,7 @@ export default function App() {
     <p>
       <strong>Program:</strong> Bachelor of Science in Business Administration Major in Human Resource Management (BSBA-HRM)<br />
       <strong>Section:</strong> BSBA-HRM-2-A<br />
-      <strong>College:</strong> College of Management, Business and Tourism
+      <strong>College:</strong> College of Management, Business and Technology
     </p>
 
     <h3 style={{ marginTop: "20px" }}>Team Members</h3>
